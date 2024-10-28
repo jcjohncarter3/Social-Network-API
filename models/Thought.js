fff2da9212015cdb18e0bd167858782d58990ecc
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-10-27 23:00:49 -0500

    updated index.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,37 @@
 const { Schema, model } = require("mongoose");
-const reactionSchema = require("./Reaction"); // Import reactionSchema
+
+// Reaction Schema
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Use a getter method to format the timestamp on query
+      get: (timestamp) => new Date(timestamp).toLocaleDateString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
 
 // Thought Schema
 const thoughtSchema = new Schema(
@@ -7,8 +39,8 @@ const thoughtSchema = new Schema(
     thoughtText: {
       type: String,
       required: true,
-      minlength: 1,
-      maxlength: 280,
+      minLength: 1,
+      maxLength: 280,
     },
     createdAt: {
       type: Date,
